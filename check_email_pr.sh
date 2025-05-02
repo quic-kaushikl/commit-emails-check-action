435ee1621a80f6bd4828b672279e22ecbed6c26c
Nasser Grainawi <nasser.grainawi@oss.qualcomm.com>
2025-05-02 10:40:15 -0700

    Fix duplicate GITHUB_REPOSITORY_OWNER
    The GITHUB_REPOSITORY variable already includes the owner.
    
    Signed-off-by: Nasser Grainawi <nasser.grainawi@oss.qualcomm.com>

--- check_email_pr.sh
+++ check_email_pr.sh
@@ -15,7 +15,7 @@ get_pr_commits() {
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $GITHUB_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY/pulls/$PULL_NUMBER/commits?per_page=$COMMITS_COUNT"
+        "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PULL_NUMBER/commits?per_page=$COMMITS_COUNT"
 }
 
 split_commits_and_add_metadata() {
